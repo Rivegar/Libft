858af706f5910368867dd9e9c608bd7b71340c07
Rivegar <alexrivgar@gmail.com>
2023-03-14 10:31:34 +0100

    Cambio añadiendo otras funciones

--- Libft/strlcpy.c
+++ Libft/strlcpy.c
@@ -13,24 +13,20 @@
 //#include "libft.h"
 #include <stdio.h>
 
-size_t ft_strlcpy(char *dest, const char *src, size_t dstsize)
+size_t ft_strlcpy(char *dest, const char *src, size_t dsize)
 {
     size_t i;
 
     i = 0;
 
-    while(i + 1 < dstsize && src[i] != '\0')
+    if(dsize > 0)
     {
-        dest[i] = src[i];
-        i++;
-    }
-    if (i < dstsize)
-    {
+        while (src[i] && i < (dsize - 1))
+        {
+            dest[i] = src[i];
+            i++;
+        }
         dest[i] = '\0';
     }
-    while(src[i] != 0)
-    {
-        i++;
-    }
-    return i;
+    return(ft_strlen(src));
 }
